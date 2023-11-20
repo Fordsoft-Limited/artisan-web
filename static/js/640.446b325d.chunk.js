31f1b24556b11c72b2a1156719e40e86bf7eb482
Ahmad Dolapo Asiyanbola <dolapo@fordsoft.tech>
2023-11-20 10:55:35 +0100

    Updates

---
+++ static/js/640.446b325d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstarter_bt5=self.webpackChunkstarter_bt5||[]).push([[640,267],{7521:(t,e,a)=>{a.r(e),a.d(e,{default:()=>l});a(2791);var s=a(7689),r=a(3172),n=a(5677),u=a(2753),d=a(184);const l=()=>{const t=(0,s.TH)(),{pathname:e}=t,{id:a}=(0,s.UO)();return(0,d.jsx)("div",{children:(0,d.jsxs)(u.default,{children:["/main"===e&&(0,d.jsx)(r.default,{}),"/main/about"===e&&(0,d.jsx)(n.default,{})]})})}}}]);
+//# sourceMappingURL=640.446b325d.chunk.js.map
