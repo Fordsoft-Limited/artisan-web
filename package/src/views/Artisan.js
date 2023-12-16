bb73be231c0b5e1fc83b1cc03499b49b17deea4d
Ahmad Dolapo Asiyanbola <dolapo@fordsoft.tech>
2023-12-16 14:18:34 +0100

    landing

--- package/src/views/Artisan.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Artisan = () => {
-  return (
-    <div>Artisan</div>
-  )
-}
-
-export default Artisan
--- package/src/views/Visitors.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Visitors = () => {
-  return (
-    <div>Visitors</div>
-  )
-}
-
-export default Visitors
